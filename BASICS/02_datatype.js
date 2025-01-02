a52527a5ec8d187b4776269a93d3e10d57193dd9
Harshit Yadav <156507632+hrst01@users.noreply.github.com>
2025-01-02 19:32:33 +0530

    js basics

---
+++ BASICS/02_datatype.js
@@ -0,0 +1,91 @@
+// Datatypes - 1. Primitive (Non reference) 2. Non Primitive (Reference)
+
+/*
+    1. Primitive Data types - 
+        a. Number = 2^53
+        b. String = set of charachters(spaces will be counted)
+        c. Boolean = true or false(0/1)
+        d. Bigint = greater 2^53
+        e. Symbol = unique data type
+*/
+
+// 1. Number 
+
+let a = 10
+const b = 20
+
+let c = a+b
+
+ console.log(c);
+
+// console.log(Number.isFinite(1/10)) // to check if the value is finite or not
+
+// let abc = new Number(20)
+
+// console.log(abc);
+
+let d = 205
+
+// console.log("Integer : ",Number.isInteger(d));
+
+// console.log(Number.isNaN("Avinash"));
+
+
+
+// 2. String
+
+let firstName = "Avinaaaash"
+let lastName = "Dubey"
+
+let fullName = firstName + lastName // string concatination 
+
+// console.log(fullName); // AvinashDubey
+
+
+// console.log(typeof firstName); // typeof is used to determine the type of a variable
+
+// console.log(firstName.length); // to determine the length of the string
+
+// 0 1 2 3 4 5 6
+// A v i n a s h
+
+// console.log(firstName.charAt(3)); // to get the value from the string based on the position given
+
+// console.log(firstName.indexOf("a")); // to get the position of given charachter
+
+// let str = "Avinash"
+
+// console.log(str.slice(3, 6)); // end value will be excluded
+
+// console.log(str.slice(-7));
+
+// let str2 = str.slice(0,5)
+
+// console.log(str2);
+// console.log(str);
+
+// let str3 = str.substring(0,4)
+
+// console.log(str3);
+// console.log(str);
+
+
+// Note -- Primitive values work on stack memory which means always a copy of original data will be passed
+// and it will never change the original value
+
+// 3. Boolean
+
+// let isValid = false
+
+// console.log(isValid);
+
+// Note -- try to use is with the variable name like isValid , isPasswordCorrect for boolean value
+
+
+// const flag1 = true
+
+// const flag2 = flag1.toString();
+// const flag3 = flag1.valueOf();
+
+// console.log(typeof flag2);
+// console.log(flag3);
---
+++ BASICS/03_nonprimitivedatatype.js
@@ -0,0 +1,159 @@
+// Non Primitive Data type 
+
+/*
+    1. Array
+    2. Object
+    3. Function
+*/
+
+//1. Array = a collection of multiple data and datatypes
+
+// let arr1 = [1, 2, 3, 4, "Sam", "Atul", true]
+
+// console.log(arr1);
+
+// console.log(arr1[1]);
+
+
+// similarity between string and array
+// let firstName = "Avinash"
+// console.log(firstName[2]);
+
+// let arr2 = arr1
+
+// console.log(arr2);
+
+// arr2 = [1,2,3,4]
+
+// console.log(arr2);
+// console.log(arr1);
+
+
+// let arr3 = [1,2,3,[1,2,[5,6,7]]]
+
+//  console.log(arr3[3][2][0]);
+//  console.log(arr3.flat(Infinity));
+
+let arr1 = [1,2,3,4,5]
+
+arr1.push(32) // add a new value at the end
+
+// console.log(arr1);
+
+arr1.pop() // remove from the end
+
+// console.log(arr1);
+
+arr1.unshift(78) // add the value at the start of the array
+
+// console.log(arr1);
+
+arr1.shift()  // remove from the start
+
+// console.log(arr1);
+
+let arr2 = [1,2,[3,4,5,[7,8,9],[4,5,6,7,[4,5,6]]]]
+
+// console.log(arr2.flat(Infinity));
+
+const array1 = [5,6, 12, 8, 130, 44];
+
+// console.log(typeof array1);
+
+const found = array1.find((element) => element > 10);//10 se badi jo bhi sabse phle value milegi usko print krega
+
+// console.log(found);
+
+
+// 2. Object = key value pair of multiple elements
+
+// let myObj = {
+//     1 : 10,
+//     b: 20,
+//     c: "Avinash",
+//     "first name" : true //
+// }
+
+// myObj["1"] = 44
+
+// console.log(myObj);
+
+
+const myObj = {
+    1 : 10,
+    b: 20,
+    c: "Avinash",
+    "first name" : true
+}
+
+// Object.freeze(myObj) // This will stop any changes in the given object
+
+
+// myObj.b = 56
+
+// console.log(Object.isFrozen(myObj));
+
+
+
+// console.log(typeof myObj);
+
+// console.log(myObj.a); // to get the value of given key
+
+// console.log(Object.values(myObj));
+
+// console.log(typeof myObj["first name"]);
+
+// 1 = "1" Type of the key in any object will always be a string no matter what the value is
+
+// console.log(Object.keys(myObj)); // this will prove that all the keys are of string type
+
+// number , Number
+// String, String
+// Boolean, Boolean
+
+// const a = new Number(22)
+
+// console.log(a);
+
+// 3. function = this is to write a block of code that can be used multiple times
+
+// function Sum(a, b) {
+//     let c = a+b;
+//     console.log(c);  
+// }
+
+// Sum(3,4)
+// Sum(4,9)
+
+// Function will have 2 parts -- 1. Body or definition 2. Call
+
+// Functions
+
+// function Sum(a,b) {
+//     let c = a+b;
+//     return c;
+//     console.log(c); // This is an unreachable code
+// }
+// Return keyword will return anything from the function.
+// But remember that anything you write after return will not be compiled
+// console.log(Sum(5,4))
+
+// Arrow function
+
+// const employee = (a, b) => {
+//   let c = a + b;
+//   return c;
+// };
+
+// console.log(employee);
+
+// Arrow function do not support "this"
+
+// Immediately Invoked Function Expression ( IIFE )
+
+// (function (a, b) {
+//   let c = a + b;
+//   console.log(c);
+// })(5, 6);
+
+// (function definition)(function call)
